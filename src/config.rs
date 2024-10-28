faa34aead67ad9d7c67621ffcef02ba237ef1942
Five <co22rkn@protonmail.com>
2024-10-28 16:13:13 +0100

    Added config file

---
+++ src/config.rs
@@ -0,0 +1,10 @@
+use std::time::Duration;
+
+pub const FIATMAXI_NSEC: &str =
+    "nsec1mq648dvh66lfn9v8rxqswtled6w779ptrev43fchdtptqfxse7xqthudsk";
+pub const FIVE_HEXPUBKEY: &str =
+    "d04ecf33a303a59852fdb681ed8b412201ba85d8d2199aec73cb62681d62aa90";
+
+pub const RELAY_CONNECTION_TIMEOUT: Duration = Duration::from_secs(4);
+pub const PUBLISH_TIMEOUT: Duration = Duration::from_secs(4);
+
