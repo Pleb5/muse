79fdb5b1560d45b0ef7e3bc93a8a0d7d5564f320
Five <co22rkn@protonmail.com>
2024-11-06 10:30:05 +0100

    *feat: WoT score calc: Add constants for wot calculation

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,9 @@ pub const FIATMAXI_NSEC: &str =
 pub const FIVE_HEXPUBKEY: &str =
     "d04ecf33a303a59852fdb681ed8b412201ba85d8d2199aec73cb62681d62aa90";
 
+pub const SATSHOOT_HEXPUBKEY: &str =
+    "e3244843f8ab6483827e305e5b9d7f61b9eb791aa274d2a36836f3999c767650";
+
 pub const RELAY_CONNECTION_TIMEOUT: Duration = Duration::from_secs(4);
 pub const PUBLISH_TIMEOUT: Duration = Duration::from_secs(4);
 
@@ -14,3 +17,14 @@ pub const BOOTSTRAP_RELAYS: [&str; 4] = [
     "wss://relay.nostr.band",
     "wss://purplepag.es",
 ];
+
+pub const MIN_WOT_SCORE: i32 = 3;
+
+pub const DIRECT_FOLLOW_WOT_SCORE: i32 = 4;
+pub const INDIRECT_FOLLOW_WOT_SCORE: i32 = 2;
+
+pub const DIRECT_MUTE_WOT_SCORE: i32 = -2;
+pub const INDIRECT_MUTE_WOT_SCORE: i32 = -1;
+
+pub const DIRECT_REPORT_WOT_SCORE: i32 = -2;
+pub const INDIRECT_REPORT_WOT_SCORE: i32 = -1;
