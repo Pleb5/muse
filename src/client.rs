1adc08872115e2ec2ab9e3cd8cd95193831cc881
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-10 14:40:28 +0100

    Improve WoT performance
    * Bump `nostr-sdk` to `v0.36`
    * Remove `Options::connection_timeout`
    * Remove unnecessary `Arc<T>`
    * Pre-allocate collection capacity where possible
    * Use `iter` or` into_iter` where possible
    * Remove unnecessary clone

--- src/client.rs
+++ src/client.rs
@@ -1,36 +1,30 @@
-use nostr_sdk::prelude::{Client, Keys, SecretKey, Options};
-use std::sync::{OnceLock, Arc};
+use std::sync::OnceLock;
 
-use crate::config;
-
+use nostr_sdk::prelude::*;
 
 // A Singleton Client instance that is shared across threads and is thread-safe
-static INSTANCE: OnceLock<Arc<Client>> = OnceLock::new();
+static INSTANCE: OnceLock<Client> = OnceLock::new();
 
 pub fn initialize_client_singleton(option: ClientBuildOption) {
     let client = create_client(option);
-    INSTANCE.set(Arc::new(client)).expect("Client Singleton already initialized!");
+    INSTANCE
+        .set(client)
+        .expect("Client Singleton already initialized!");
 }
 
-pub fn get_client() -> Arc<Client> {
-    INSTANCE.get().expect("Client Singleton NOT initialized!").clone()
+pub fn get_client() -> &'static Client {
+    INSTANCE.get().expect("Client Singleton NOT initialized!")
 }
 
 fn create_client(option: ClientBuildOption) -> Client {
-    let opts = Options::new()
-        .gossip(true)
-        .connection_timeout(Some(config::RELAY_CONNECTION_TIMEOUT))
-        .send_timeout(Some(config::PUBLISH_TIMEOUT));
+    let opts = Options::new().gossip(true);
 
     match option {
         ClientBuildOption::WithNsec(secret_key) => {
-            let my_keys = Keys::new(secret_key);
-
-            Client::with_opts(&my_keys, opts)
+            let keys = Keys::new(secret_key);
+            Client::builder().signer(keys).opts(opts).build()
         }
-        ClientBuildOption::NoNsec => {
-            Client::builder().opts(opts).build()
-        }
+        ClientBuildOption::NoNsec => Client::builder().opts(opts).build(),
     }
 }
 
--- src/wot.rs
+++ src/wot.rs
@@ -3,16 +3,13 @@ use nostr_sdk::prelude::*;
 use crate::client::get_client;
 
 use crate::config::{
-    MIN_WOT_SCORE,
-    DIRECT_FOLLOW_WOT_SCORE, INDIRECT_FOLLOW_WOT_SCORE,
-    DIRECT_MUTE_WOT_SCORE, INDIRECT_MUTE_WOT_SCORE,
-    DIRECT_REPORT_WOT_SCORE, INDIRECT_REPORT_WOT_SCORE
+    DIRECT_FOLLOW_WOT_SCORE, DIRECT_MUTE_WOT_SCORE, DIRECT_REPORT_WOT_SCORE,
+    INDIRECT_FOLLOW_WOT_SCORE, INDIRECT_MUTE_WOT_SCORE, INDIRECT_REPORT_WOT_SCORE, MIN_WOT_SCORE,
 };
 
-use std::time::Duration;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicPtr, Ordering};
 use std::collections::HashMap;
+use std::time::Duration;
+
 use dashmap::DashSet;
 use once_cell::sync::Lazy;
 
@@ -20,31 +17,24 @@ const KEY_TO_FOLLOWS: &str = "follows";
 const KEY_TO_MUTES: &str = "mutes";
 const KEY_TO_REPORTS: &str = "reports";
 
-pub static WOT: Lazy<Arc<DashSet<PublicKey>>> = Lazy::new(|| {
-    Arc::new(DashSet::new())
-});
+pub static WOT: Lazy<DashSet<PublicKey>> = Lazy::new(DashSet::new);
 
 pub async fn update_wot(user_pubkey: &PublicKey) -> Result<(), Error> {
     let mut network_wot_scores: HashMap<PublicKey, i32> = HashMap::new();
 
-    let mut authors = vec![user_pubkey.clone()];
+    let mut authors = vec![*user_pubkey];
 
-    let user_wot_events_map = fetch_wot_events_of_users(authors.clone())
-        .await?;
+    let user_wot_events_map = fetch_wot_events_of_users(authors.clone()).await?;
 
     authors.clear();
 
     if let Some(follow_events) = user_wot_events_map.get(KEY_TO_FOLLOWS) {
-        for follow_event in follow_events {
-            for tag in follow_event.tags.iter() {
-                if let Some(TagStandard::PublicKey {
-                    public_key,
-                    .. 
-                }) = tag.clone().to_standardized()
-                {
-                    authors.push(public_key);
-                }
-            }
+        for follow_event in follow_events.iter() {
+            // Reserve capacity
+            authors.reserve_exact(follow_event.tags.public_keys().count());
+
+            // Extend
+            authors.extend(follow_event.tags.public_keys());
         }
     }
 
@@ -53,21 +43,20 @@ pub async fn update_wot(user_pubkey: &PublicKey) -> Result<(), Error> {
         user_wot_events_map,
         DIRECT_FOLLOW_WOT_SCORE,
         DIRECT_MUTE_WOT_SCORE,
-        DIRECT_REPORT_WOT_SCORE
+        DIRECT_REPORT_WOT_SCORE,
     );
 
-    let follows_wot_events_map = fetch_wot_events_of_users(authors.clone())
-        .await?;
+    let follows_wot_events_map = fetch_wot_events_of_users(authors.clone()).await?;
 
     update_wot_scores(
         &mut network_wot_scores,
         follows_wot_events_map,
         INDIRECT_FOLLOW_WOT_SCORE,
         INDIRECT_MUTE_WOT_SCORE,
-        INDIRECT_REPORT_WOT_SCORE
+        INDIRECT_REPORT_WOT_SCORE,
     );
 
-    for (public_key, wot_score) in network_wot_scores {
+    for (public_key, wot_score) in network_wot_scores.into_iter() {
         if wot_score >= MIN_WOT_SCORE {
             WOT.insert(public_key);
         }
@@ -77,9 +66,9 @@ pub async fn update_wot(user_pubkey: &PublicKey) -> Result<(), Error> {
 }
 
 pub async fn fetch_wot_events_of_users(
-    user_pubkeys: Vec<PublicKey>
-) -> Result<HashMap<String, Vec<Event>>, Error> {
-    let mut wot_events_map: HashMap<String, Vec<Event>> = HashMap::new();
+    user_pubkeys: Vec<PublicKey>,
+) -> Result<HashMap<&'static str, Vec<Event>>, Error> {
+    let mut wot_events_map: HashMap<&'static str, Vec<Event>> = HashMap::new();
 
     let filter: Filter = Filter::new()
         .authors(user_pubkeys.iter().cloned())
@@ -89,56 +78,51 @@ pub async fn fetch_wot_events_of_users(
 
     let fetch_timeout = Duration::from_secs(5);
 
-    match get_client()
-        .get_events_of(vec![filter], EventSource::relays(Some(fetch_timeout)))
-        .await
-    {
-        Ok(wot_events) => {
-            for wot_event in wot_events {
-                match wot_event.kind {
-                    Kind::ContactList => wot_events_map
-                        .entry(KEY_TO_FOLLOWS.to_string())
-                        .or_insert(Vec::new())
-                        .push(wot_event),
+    let wot_events = get_client()
+        .fetch_events(vec![filter], Some(fetch_timeout))
+        .await?;
 
-                    Kind::MuteList => wot_events_map
-                        .entry(KEY_TO_MUTES.to_string())
-                        .or_insert(Vec::new())
-                        .push(wot_event),
+    for wot_event in wot_events.into_iter() {
+        match wot_event.kind {
+            Kind::ContactList => wot_events_map
+                .entry(KEY_TO_FOLLOWS)
+                .or_default()
+                .push(wot_event),
 
-                    Kind::Reporting => wot_events_map
-                        .entry(KEY_TO_REPORTS.to_string())
-                        .or_insert(Vec::new())
-                        .push(wot_event),
+            Kind::MuteList => wot_events_map
+                .entry(KEY_TO_MUTES)
+                .or_default()
+                .push(wot_event),
 
-                    _ => ()
-                }
-            }
-            Ok(wot_events_map)
+            Kind::Reporting => wot_events_map
+                .entry(KEY_TO_REPORTS)
+                .or_default()
+                .push(wot_event),
+
+            _ => (),
         }
-        Err(e) => Err(e)
     }
+    Ok(wot_events_map)
 }
 
 fn update_wot_scores(
     network_wot_scores: &mut HashMap<PublicKey, i32>,
-    wot_events_map: HashMap<String, Vec<Event>>,
+    wot_events_map: HashMap<&'static str, Vec<Event>>,
     follow_wot_score: i32,
     mute_wot_score: i32,
-    report_wot_score: i32
+    report_wot_score: i32,
 ) {
-
     if let Some(follow_events) = wot_events_map.get(KEY_TO_FOLLOWS) {
-        for follow_event in follow_events {
+        for follow_event in follow_events.iter() {
             for tag in follow_event.tags.iter() {
                 if let Some(TagStandard::PublicKey {
                     public_key,
-                    uppercase:false,
-                    .. 
-                }) = tag.clone().to_standardized()
+                    uppercase: false,
+                    ..
+                }) = tag.as_standardized()
                 {
                     *network_wot_scores
-                        .entry(public_key)
+                        .entry(*public_key)
                         .or_insert(follow_wot_score) += follow_wot_score;
                 }
             }
@@ -146,16 +130,16 @@ fn update_wot_scores(
     }
 
     if let Some(mute_events) = wot_events_map.get(KEY_TO_MUTES) {
-        for mute_event in mute_events {
+        for mute_event in mute_events.iter() {
             for tag in mute_event.tags.iter() {
                 if let Some(TagStandard::PublicKey {
                     public_key,
-                    uppercase:false,
-                    .. 
-                }) = tag.clone().to_standardized()
+                    uppercase: false,
+                    ..
+                }) = tag.as_standardized()
                 {
                     *network_wot_scores
-                        .entry(public_key)
+                        .entry(*public_key)
                         .or_insert(mute_wot_score) += mute_wot_score;
                 }
             }
@@ -164,9 +148,9 @@ fn update_wot_scores(
 
     if let Some(report_events) = wot_events_map.get(KEY_TO_REPORTS) {
         for report_event in report_events {
-            if let Some(public_key) = report_event.public_keys().next() {
+            if let Some(public_key) = report_event.tags.public_keys().next() {
                 *network_wot_scores
-                    .entry(public_key.clone())
+                    .entry(*public_key)
                     .or_insert(mute_wot_score) += report_wot_score;
             }
         }
